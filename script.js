4c669972bebcee81a18931ac18a8d50ba20b6a96
aiydn <48388034+aiydn@users.noreply.github.com>
2023-01-22 19:07:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+window.onresize = function () { reset(); }
+var calc
+const gcd = (...arr) => {
+  const _gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  return [...arr].reduce((a, b) => _gcd(a, b));
+};
+const lcm = (...arr) => {
+  const gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  const _lcm = (x, y) => (x * y) / gcd(x, y);
+  return [...arr].reduce((a, b) => _lcm(a, b));
+};
 var img = getImg(294, 150);
 var dvd = getDvd();
 var screen = getScreen();
@@ -6,31 +17,19 @@ var logo = getGoodStart();
 var screen = getScreen();
 var colorH = 0;
 var logoColor = getColor();
-const dvdLogo = new Image();
-var future = logo;
-var logPX = [0];
-var logHit = [0];
-var countdown = 0;
 var recentHit = false;
-var speed = getSpeed(2);
-var oldSpeed = speed;
+var speed = getSpeed(1);
 var startTime = performance.now();
-var resizeTimeOut;
-window.onresize = function () {
-  clearTimeout(resizeTimeOut);
-  resizeTimeOut = setTimeout(function () {
-    reset("WINDOWS RESIZE RESET"); // run resize function
-  }, 1000); // time in ms for not resized
-};
-var type = "time";
+var maxpx = (lcm(max.x, max.y));
+var corner = nextCorner(logo);
 
 wakeLock();
 animate();
-requestAnimationFrame(calc);
+
 setScreen();
 
 document.addEventListener("visibilitychange", () => {
-  if (document.visibilityState = 'visible') { reset("WINDOWS INACTIVE RESET") }
+  if (document.visibilityState = 'visible') { reset() }
 })
 
 async function wakeLock() {
@@ -42,20 +41,14 @@ async function wakeLock() {
   }
 }
 
+function reCalc() { console.log("reCalc"); corner = nextCorner(logo) }
 function getGoodStart() {
   let x = getRndInteger(0, window.innerWidth - 256 - 1),
     y = getRndInteger(0, window.innerHeight - 147 - 1),
     vx = oneOf2(1, -1),
-    vy = oneOf2(1, -1);
-  while (willEverHit(x, y) == false) {
-    console.log("getting another start")
-    x = getRndInteger(0, window.innerWidth - 256 - 1),
-      y = getRndInteger(0, window.innerHeight - 147 - 1),
-      vx = oneOf2(1, -1),
-      vy = oneOf2(1, -1);
-  };
-  let px = 0;
-  let hit = 0;
+    vy = oneOf2(1, -1),
+    px = 0,
+    hit = false;
   return { x, y, vx, vy, px, hit };
 }
 function getSpeed(s) {
@@ -70,15 +63,14 @@ function getScreen() {
 
 function getMax() {
   let x = window.innerWidth - img.w,
-    y = window.innerHeight - img.h
-  px = lcm(x, y)
-  return { x, y, px }
+    y = window.innerHeight - img.h;
+  return { x, y }
 }
 
 function getImg(srcW, srcH) {
 
-  let w = Math.round(Math.min(window.innerHeight, window.innerWidth) / 1000 * srcW);
-  let h = Math.round(Math.min(window.innerHeight, window.innerWidth) / 1000 * srcH);
+  let w = Math.round(Math.min(window.innerHeight, window.innerWidth) / 1000 * srcW),
+    h = Math.round(Math.min(window.innerHeight, window.innerWidth) / 1000 * srcH);
   return { w, h }
 }
 
@@ -90,7 +82,6 @@ function getDvd() {
 
 function getRndInteger(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min }
 function oneOf2(option0, option1) { if (Math.round(Math.random()) == 1) { return option0 } else { return option1 } }
-function willEverHit(x, y) { if (Math.abs(x - y) % gcd(max.x, max.y) == 0) { return true } else { return false } }
 
 function getColor() {
   let h = colorH
@@ -103,41 +94,21 @@ function getColor() {
 }
 
 function time(px) {
-  let hitTime = new Date(new Date().getTime() + ((performance.now() - startTime) / logo.px * px));
-  return hitTime.toLocaleString()
+  let hitTime = new Date(((performance.now() - startTime) / logo.px * px));
+  return { H: padWithLeadingZeros(hitTime.getHours(), 2), M: padWithLeadingZeros(hitTime.getMinutes(), 2), S: padWithLeadingZeros(hitTime.getSeconds(), 2), MS: padWithLeadingZeros(hitTime.getMilliseconds(), 3) }
 }
 
-function gcd(a, b) {
-  var temp;
-  if (a < 0) { a = -a; };
-  if (b < 0) { b = -b; };
-  if (b > a) { temp = a; a = b; b = temp; };
-  while (true) {
-    a %= b;
-    if (a == 0) { return b; };
-    b %= a;
-    if (b == 0) { return a; };
-  };
-  return b;
-}
-
-function lcm(a, b) {
-  return Math.abs(a * b) / gcd(a, b);
-}
-
-function calc() {
-  if (willEverHit(logo.x, logo.y) == false) { type = "never"}
-  else {
-    while (typeof logPX[logo.hit + 1] == 'undefined') {
-      future = nextWall(future.x, future.y, future.vx, future.vy, future.px, future.hit)
-    }
+function nextCorner(future) {
+  for (let run = 0; run < maxpx; run++) {
+    future = nextWall(future.x, future.y, future.vx, future.vy, future.px, future.hit);
+    if (future.hit == true) { return { hit: future.hit, corner: whichCorner(future.x, future.y), px: future.px }; }
   }
+  return { hit: future.hit, corner: whichCorner(future.x, future.y), px: future.px };
 }
 
 function nextWall(x, y, vx, vy, px, hit) {
-  let oldHit = hit
-  let oldVX = vx
-  let oldVY = vy
+  let oldVX = vx;
+  let oldVY = vy;
   var distancex, distancey, plus
   if (oldVX == 1) { distancex = max.x - x; } //right
   else { distancex = x; }; //left
@@ -145,30 +116,16 @@ function nextWall(x, y, vx, vy, px, hit) {
   else { distancey = y; }; //top
   if (distancex > distancey) { vy = -1 * oldVY; plus = Math.abs(distancey); }
   else if (distancey > distancex) { vx = -1 * oldVX; plus = Math.abs(distancex); }
-  else { vx = -1 * oldVX; vy = -1 * oldVY; plus = Math.abs(distancex); hit += 1; };
+  else { vx = -1 * oldVX; vy = -1 * oldVY; plus = Math.abs(distancex); hit = true; };
   x += oldVX * plus;
   y += oldVY * plus;
   px += plus;
-  if (oldHit !== hit) { addHit(x, y, px, hit); };
   return { x, y, vx, vy, px, hit }
 }
 
-function changevx(x, vx, max) {
-  if (x == max) { return -vx }
-  if (x == 0) { return -vx }
-}
-
-function changevy(y, vy, max) {
-  if (y == max) { return -vy }
-  if (y == 0) { return -vy }
-}
+function check(xy, maxXY) { if (max[maxXY] - (Math.abs(xy * 2 - max[maxXY])) == 0) { return true } }
 
 function darkLight(dark, light) { if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) { return dark } else { return light } }
-function addHit(x, y, px, hit) {
-  logPX.push(px)
-  logHit.push(whichCorner(x, y))
-  console.log("corner: " + whichCorner(x, y) + ", px: " + px + ", hit number: " + hit);
-}
 
 function whichCorner(x, y) {
   if (x == 0 && y == 0) { return 'top left'; }
@@ -182,12 +139,12 @@ function animate() {
   for (var i = 0; i < speed; i++) {
     logo.x += logo.vx;
     logo.y += logo.vy;
-    if (changevx(logo.x, logo.vx, max.x) !== undefined) { logo.vx = changevx(logo.x, logo.vx, max.x); logoColor = getColor(); recentHit = false };
-    if (changevy(logo.y, logo.vy, max.y) !== undefined) { logo.vy = changevy(logo.y, logo.vy, max.y); logoColor = getColor(); recentHit = false };
-    if (recentHit) { logoColor = getColor() }
-    logo.px += 1
-    if (whichCorner(logo.x, logo.y) !== undefined) { recentHit = true; logo.hit += 1; requestAnimationFrame(calc) }
-    stats()
+    if (check(logo.x, "x")) { logo.vx = -logo.vx; logoColor = getColor(); recentHit = false };
+    if (check(logo.y, "y")) { logo.vy = -logo.vy; logoColor = getColor(); recentHit = false };
+    if (recentHit) { logoColor = getColor() };
+    logo.px += 1;
+    if (whichCorner(logo.x, logo.y) !== undefined) { recentHit = true; logo.hit += 1; reset() };
+    stats();
   }
   draw()
 }
@@ -195,6 +152,7 @@ function animate() {
 function draw() {
   var canvas = document.getElementById("c");
   var context = canvas.getContext("2d");
+  const dvdLogo = new Image();
   dvdLogo.src = darkLight(dvd.black, dvd.white)
   context.clearRect(0, 0, screen.w, screen.h);
   context.fillStyle = darkLight("#000000", "#ffffff");
@@ -208,24 +166,25 @@ function fix() {
   if (logo.x > max.x) { logo.x = max.x; logo.vx = -1 }
   if (logo.y > max.y) { logo.y = max.y; logo.vy = -1 }
   if (logo.x < 0) { logo.x = 0; logo.vx = 1 }
-  if (logo.y < 0) { logo.y = 0; logo.vx = 1 }
+  if (logo.y < 0) { logo.y = 0; logo.vy = 1 }
 }
 
-function reset(log) {
+function reset() {
   img = getImg(294, 150);
   max = getMax();
   screen = getScreen();
   speed = getSpeed(2)
   setScreen();
   fix();
-  logPX = [0];
-  logHit = [0];
   startTime = performance.now();
   logo.px = 0;
-  future = logo;
-  requestAnimationFrame(calc);
-  console.log(log)
+  clearTimeout(calc);
+  calc = setTimeout(function () {
+    reCalc();
+  }, 1000);
+  console.log("RESET");
 }
+
 function setScreen() {
   var ctx = document.getElementById("c").getContext("2d");
   ctx.canvas.width = screen.w;
@@ -233,16 +192,13 @@ function setScreen() {
 }
 
 function stats() {
-  let output;
-  if (typeof logPX[logo.hit + 1] !== 'undefined') {
-    let hit = logHit[logo.hit + 1];
-    let px = logPX[logo.hit + 1] - logo.px;
-    if (type == "time") { output =  "The " + hit + " corner will be hit at " + time(px)}
-    else if (type == "pixels") { output = "The " + hit + " corner will be hit in " + px + " pixels" };
+  let output
+  if (corner.hit == true) {
+    let countdown = time(corner.px - logo.px);
+    output = "It will take " + countdown.H + " hour " + countdown.M + " minutes " + countdown.S + " seconds until the " + corner.corner + " corner will be hit"
   }
-  else if (type == "never") { output = "The corner will never be hit" }
-  else { output = " " }
-  if (output !== document.getElementById("stats").innerText) {document.getElementById("stats").innerText = output}
+  else if (corner.hit == false) { output = "The corner will never be hit" }
+  if (output !== document.getElementById("stats").innerText) { document.getElementById("stats").innerText = output }
 }
 
 function chromiumOrElse(chromium, orElse) {
@@ -252,6 +208,12 @@ function chromiumOrElse(chromium, orElse) {
 }
 
 function turbo() {
-  if (logPX[logo.hit + 1] - logo.px < 1000) { reset("TURBO DONE RESET"); type = "time" }
-  else { speed = 300; requestAnimationFrame(turbo); type = "pixels" }
+  if (corner.px - logo.px < 1000) { reset() }
+  else { speed = 300; requestAnimationFrame(turbo) }
 }
+
+function padWithLeadingZeros(num, totalLength) {
+  return String(num).padStart(totalLength, '0');
+}
+
+function willEverHit(x, y) { if (Math.abs(x - y) % gcd(max.x, max.y) == 0) { return true } else { return false } }
